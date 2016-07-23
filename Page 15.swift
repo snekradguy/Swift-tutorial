1e9792e82587d8a53f6fef771554135a1b3c17a7
snekradguy <snekradguy@gmail.com>
2016-07-23 17:38:26 +0200

    Experiment

--- Page 15.swift
+++ Page 15.swift
@@ -4,11 +4,13 @@ let  interestingNumbers = [
     "Square":[1, 4, 9, 16, 25],
 ]
 var largest = 0
+var kindOfLargest: String = String()
 for (kind, numbers) in interestingNumbers {
     for number in numbers {
         if number > largest {
             largest = number
+            kindOfLargest = kind
         }
     }
 }
-print(largest)
+print(largest, kindOfLargest)
