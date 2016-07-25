0bd571beb73954ecf362acd5c1e600e0a55c3f16
snekradguy <snekradguy@gmail.com>
2016-07-25 19:37:40 +0200

    Own experiment

--- Page 17 2.swift
+++ Page 17 2.swift
@@ -1,4 +1,4 @@
 func greet(person: String, day: String) -> String {
     return "Hello \(person), today is \(day)."
 }
-greet(person: "Bob", day: "Tuesday")
+print(greet(person: "Bob", day: "Tuesday"))
