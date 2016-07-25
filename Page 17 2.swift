fa438095519895f28bc5f6feda917ea842224779
snekradguy <snekradguy@gmail.com>
2016-07-25 20:04:17 +0200

    Own addition

--- Page 17 2.swift
+++ Page 17 2.swift
@@ -1,4 +1,4 @@
 func greet(person: String, lunch: String) -> String {
     return "Hello \(person), today's special lunch is \(lunch)."
 }
-greet(person: "Bob", lunch: "Egg custard tart")
+print(greet(person: "Bob", lunch: "Egg custard tart"))
