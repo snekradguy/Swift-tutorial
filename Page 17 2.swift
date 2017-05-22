14353b1a437071d1c0f645984316b3e11ac3f7e6
snekradguy <snekradguy@gmail.com>
2017-05-22 19:45:29 +0200

    Restart
    This reverts commit 2598c4b73836d9c12ea5a96af008ad39c9c58b4f.

--- Page 17 2.swift
+++
@@ -1,4 +0,0 @@
-func greet(person: String, lunch: String) -> String {
-    return "Hello \(person), today's lunch special is \(lunch)."
-}
-print(greet(person: "Bob", lunch: "egg custard tart"))
--- Page 20.swift
+++
@@ -1,9 +0,0 @@
-func averageOf(numbers: Int...) -> Int {
-    var average = 0
-    for number in numbers {
-        average += number
-    }
-    return average
-}
-averageOf()
-averageOf(numbers: 42, 597, 12)
